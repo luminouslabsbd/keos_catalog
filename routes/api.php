420feff06104e94f16db0eb6352fc9efb5250585
Milon Kumar <89747399+milon-kumar@users.noreply.github.com>
2024-03-26 16:10:01 +0600

    Update Api

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/products',[ProductController::class,'index']);
 Route::post('/send-wp-message',[ProductController::class,'sendWpMessage']);
+
+Route::get('/test',function(){
+   return "Welcome to keos desk catalog";
+});
