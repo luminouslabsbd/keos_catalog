358cd61e4e47d86c041db840801232131018fb5e
Milon Kumar <89747399+milon-kumar@users.noreply.github.com>
2024-03-26 21:53:45 +0600

    Update

--- app/Service/ApiService.php
+++ app/Service/ApiService.php
@@ -6,14 +6,9 @@ class ApiService
 {
     public function sendSmsInWhatsapp($data)
     {
-
-// API endpoint URL
         $url = "https://api.gupshup.io/sm/api/v1/msg";
-
-// API key
         $apiKey = "cky6px6gylnajx0epf1xafnxqluh8lyh";
 
-// Data to be sent
         $data = array(
             'channel' => 'WhatsApp',
             'source' => '573022177303',
@@ -32,10 +27,8 @@ class ApiService
             )),
             'src.name' => 'CoKeos4App'
         );
-// Initialize cURL
         $ch = curl_init($url);
 
-// Set cURL options
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
@@ -44,19 +37,12 @@ class ApiService
             'apikey: ' . $apiKey
         ));
 
-// Execute the request
         $response = curl_exec($ch);
-
-// Check for errors
         if(curl_error($ch)) {
             echo 'Error: ' . curl_error($ch);
         }
-
-// Close cURL session
         curl_close($ch);
-
-// Print the response
-        echo $response;
+        return $response;
     }
 
 
