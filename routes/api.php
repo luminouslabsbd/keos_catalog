e8975d6a2458f40d59ea9400a092cb9b42db6f9b
Milon Kumar <89747399+milon-kumar@users.noreply.github.com>
2024-03-27 21:36:22 +0600

    Update

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/products',[ProductController::class,'index']);
 Route::post('/send-wp-message',[ProductController::class,'sendWpMessage']);
 
-Route::get('/test',[ProductController::class,'store']);
